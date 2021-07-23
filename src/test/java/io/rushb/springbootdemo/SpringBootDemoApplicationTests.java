78383bb18581e78d6e0675e54cae9f1b9126a9c5
FLAMINGO <flamingodev@outlook.com>
2021-07-24 00:37:36 +0800

    update order no

--- src/test/java/io/rushb/springbootdemo/SpringBootDemoApplicationTests.java
+++ src/test/java/io/rushb/springbootdemo/SpringBootDemoApplicationTests.java
@@ -1,13 +1,35 @@
 package io.rushb.springbootdemo;
 
+import cn.hutool.core.date.DateUtil;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.Date;
+import java.util.Random;
 
 @SpringBootTest
 class SpringBootDemoApplicationTests {
 
     @Test
     void contextLoads() {
+    }
+
+    @Test
+    void testOrderNo() {
+        String orderCode = "NO";
+        String currDateTimeStr = DateUtil.format(new Date(), "yyyyMMddHHmmssSSS");
+        // need jdk 11+
+        String prefix = currDateTimeStr.substring(2);
+        Random random = new Random();
+        String randomInt = String.valueOf(random.nextInt(90) + 10);
+        String no = orderCode.concat(prefix).concat(randomInt);
+        System.out.println(currDateTimeStr);
+
+        System.out.println(orderCode);
+        System.out.println(prefix);
+        System.out.println(randomInt);
+
+        System.out.println(no);
     }
 
 }
